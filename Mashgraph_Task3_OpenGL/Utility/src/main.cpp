2b9fd8f3fbde670f26a91c979592409d9989bd19
igor-anferov <igor-anferov@mail.ru>
2016-11-27 21:20:59 +0300

    DONE

--- Mashgraph_Task3_OpenGL/Utility/src/main.cpp
+++ Mashgraph_Task3_OpenGL/Utility/src/main.cpp
@@ -17,28 +17,21 @@ class Shader {
 public:
     GLuint Program;
     Shader(){};
-    // Constructor generates the shader on the fly
     Shader(const GLchar* vertexPath, const GLchar* fragmentPath) {
-        // 1. Retrieve the vertex/fragment source code from filePath
         std::string vertexCode;
         std::string fragmentCode;
         std::ifstream vShaderFile;
         std::ifstream fShaderFile;
-        // ensures ifstream objects can throw exceptions:
         vShaderFile.exceptions (std::ifstream::badbit);
         fShaderFile.exceptions (std::ifstream::badbit);
         try {
-            // Open files
             vShaderFile.open(vertexPath);
             fShaderFile.open(fragmentPath);
             std::stringstream vShaderStream, fShaderStream;
-            // Read file's buffer contents into streams
             vShaderStream << vShaderFile.rdbuf();
             fShaderStream << fShaderFile.rdbuf();
-            // close file handlers
             vShaderFile.close();
             fShaderFile.close();
-            // Convert stream into string
             vertexCode = vShaderStream.str();
             fragmentCode = fShaderStream.str();
         }
@@ -47,47 +40,38 @@ public:
         }
         const GLchar* vShaderCode = vertexCode.c_str();
         const GLchar * fShaderCode = fragmentCode.c_str();
-        // 2. Compile shaders
         GLuint vertex, fragment;
         GLint success;
         GLchar infoLog[512];
-        // Vertex Shader
         vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode, NULL);
         glCompileShader(vertex);
-        // Print compile errors if any
         glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
         if (!success) {
             glGetShaderInfoLog(vertex, 512, NULL, infoLog);
             std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
         }
-        // Fragment Shader
         fragment = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragment, 1, &fShaderCode, NULL);
         glCompileShader(fragment);
-        // Print compile errors if any
         glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
         if (!success) {
             glGetShaderInfoLog(fragment, 512, NULL, infoLog);
             std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
         }
-        // Shader Program
         this->Program = glCreateProgram();
         glAttachShader(this->Program, vertex);
         glAttachShader(this->Program, fragment);
         glLinkProgram(this->Program);
-        // Print linking errors if any
         glGetProgramiv(this->Program, GL_LINK_STATUS, &success);
         if (!success) {
             glGetProgramInfoLog(this->Program, 512, NULL, infoLog);
             std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
         }
-        // Delete the shaders as they're linked into our program now and no longer necessery
         glDeleteShader(vertex);
         glDeleteShader(fragment);
         
     }
-    // Uses the current shader
     void Use() {
         glUseProgram(this->Program);
     }
@@ -103,16 +87,14 @@ struct Vertex {
 struct Texture {
     GLuint id;
     string type;
-    aiString path; // We store the path of the texture to compare with other textures
+    aiString path;
 };
 
 class Mesh {
 public:
-    /* Mesh Data */
     vector<Vertex> vertices;
     vector<GLuint> indices;
     vector<Texture> textures;
-    /* Functions */
     Mesh(vector<Vertex> vertices, vector<GLuint> indices, vector<Texture> textures) {
         this->vertices = vertices;
         this->indices = indices;
@@ -123,29 +105,25 @@ public:
         GLuint diffuseNr = 1;
         GLuint specularNr = 1;
         for(GLuint i = 0; i < this->textures.size(); i++) {
-            glActiveTexture(GL_TEXTURE0 + i); // Activate proper texture unit before binding
-            // Retrieve texture number (the N in diffuse_textureN)
+            glActiveTexture(GL_TEXTURE0 + i);
             stringstream ss;
             string number;
             string name = this->textures[i].type;
             if(name == "texture_diffuse")
-                ss << diffuseNr++; // Transfer GLuint to stream
+                ss << diffuseNr++;
             else if(name == "texture_specular")
-                ss << specularNr++; // Transfer GLuint to stream
+                ss << specularNr++;
             number = ss.str();
             glUniform1f(glGetUniformLocation(shader.Program, ("material." + name + number).c_str()), i);
             glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
         }
         glActiveTexture(GL_TEXTURE0);
-        // Draw mesh
         glBindVertexArray(this->VAO);
         glDrawElementsInstanced(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, 0, instance_count);
         glBindVertexArray(0);
     };
-    /* Render data */
     GLuint VAO, VBO, EBO;
 private:
-    /* Functions    */
     void setupMesh() {
         glGenVertexArrays(1, &this->VAO);
         glGenBuffers(1, &this->VBO);
@@ -155,13 +133,10 @@ private:
         glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(Vertex), &this->vertices[0], GL_STATIC_DRAW);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(GLuint), &this->indices[0], GL_STATIC_DRAW);
-        // Vertex Positions
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
-        // Vertex Normals
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Normal));
-        // Vertex Texture Coords
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, TexCoords));
         glBindVertexArray(0);
@@ -169,19 +144,16 @@ private:
 };
 
 GLint TextureFromFile(const char* path, string directory) {
-    //Generate texture ID and load texture data
     string filename = string(path);
     filename = directory + '/' + filename;
     GLuint textureID;
     glGenTextures(1, &textureID);
     int width,height;
     unsigned char* image = SOIL_load_image(filename.c_str(), &width, &height, 0, SOIL_LOAD_RGBA);
-    // Assign texture to ID
     glBindTexture(GL_TEXTURE_2D, textureID);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     
-    // Parameters
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
@@ -193,53 +165,36 @@ GLint TextureFromFile(const char* path, string directory) {
 
 class Model {
 public:
-    /*  Functions   */
-    // Constructor, expects a filepath to a 3D model.
     Model(){};
     Model(GLchar* path) {
         this->loadModel(path);
     }
-    
-    // Draws the model, and thus all its meshes
     void Draw(Shader shader, int instance_count = 1) {
         for(GLuint i = 0; i < this->meshes.size(); i++)
         this->meshes[i].Draw(shader, instance_count);
     }
     
-    /*  Model Data  */
     vector<Mesh> meshes;
 private:
     string directory;
-    vector<Texture> textures_loaded;	// Stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+    vector<Texture> textures_loaded;
     
-    /*  Functions   */
-    // Loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
     void loadModel(string path) {
-        // Read file via ASSIMP
         Assimp::Importer importer;
         const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
-        // Check for errors
         if(!scene || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) { // if is Not Zero
             cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
             return;
         }
-        // Retrieve the directory path of the filepath
         this->directory = path.substr(0, path.find_last_of('/'));
-        
-        // Process ASSIMP's root node recursively
         this->processNode(scene->mRootNode, scene);
     }
     
-    // Processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
     void processNode(aiNode* node, const aiScene* scene) {
-        // Process each mesh located at the current node
         for(GLuint i = 0; i < node->mNumMeshes; i++) {
-            // The node object only contains indices to index the actual objects in the scene.
-            // The scene contains all the data, node is just to keep stuff organized (like relations between nodes).
             aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
             this->meshes.push_back(this->processMesh(mesh, scene));
         }
-        // After we've processed all of the meshes (if any) we then recursively process each of the children nodes
         for(GLuint i = 0; i < node->mNumChildren; i++) {
             this->processNode(node->mChildren[i], scene);
         }
@@ -247,33 +202,25 @@ private:
     }
     
     Mesh processMesh(aiMesh* mesh, const aiScene* scene) {
-        // Data to fill
         vector<Vertex> vertices;
         vector<GLuint> indices;
         vector<Texture> textures;
         
-        // Walk through each of the mesh's vertices
         for(GLuint i = 0; i < mesh->mNumVertices; i++) {
             Vertex vertex;
-            VM::vec3 vector; // We declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to VM's vec3 class so we transfer the data to this placeholder VM::vec3 first.
-            // Positions
+            VM::vec3 vector;
             vector.x = mesh->mVertices[i].x;
             vector.y = mesh->mVertices[i].y;
             vector.z = mesh->mVertices[i].z;
             vertex.Position = vector;
-            // Normals
             if (mesh->mNormals) {
                 vector.x = mesh->mNormals[i].x;
                 vector.y = mesh->mNormals[i].y;
                 vector.z = mesh->mNormals[i].z;
             }
             vertex.Normal = vector;
-            // Texture Coordinates
-            if(mesh->mTextureCoords[0]) // Does the mesh contain texture coordinates?
-            {
+            if(mesh->mTextureCoords[0]) {
                 VM::vec2 vec;
-                // A vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't
-                // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
                 vec.x = mesh->mTextureCoords[0][i].x;
                 vec.y = mesh->mTextureCoords[0][i].y;
                 vertex.TexCoords = vec;
@@ -282,73 +229,47 @@ private:
                 vertex.TexCoords = VM::vec2(0.0f, 0.0f);
             vertices.push_back(vertex);
         }
-        // Now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
         for(GLuint i = 0; i < mesh->mNumFaces; i++) {
             aiFace face = mesh->mFaces[i];
-            // Retrieve all indices of the face and store them in the indices vector
             for(GLuint j = 0; j < face.mNumIndices; j++)
                 indices.push_back(face.mIndices[j]);
         }
-        // Process materials
         if(mesh->mMaterialIndex >= 0) {
             aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-            // We assume a convention for sampler names in the shaders. Each diffuse texture should be named
-            // as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER.
-            // Same applies to other texture as the following list summarizes:
-            // Diffuse: texture_diffuseN
-            // Specular: texture_specularN
-            // Normal: texture_normalN
-            
-            // 1. Diffuse maps
             vector<Texture> diffuseMaps = this->loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
             textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-            // 2. Specular maps
             vector<Texture> specularMaps = this->loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
             textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
         }
         
-        // Return a mesh object created from the extracted mesh data
         return Mesh(vertices, indices, textures);
     }
     
-    // Checks all material textures of a given type and loads the textures if they're not loaded yet.
-    // The required info is returned as a Texture struct.
     vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName) {
         vector<Texture> textures;
         for(GLuint i = 0; i < mat->GetTextureCount(type); i++) {
             aiString str;
             mat->GetTexture(type, i, &str);
-            // Check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
             GLboolean skip = false;
             for(GLuint j = 0; j < textures_loaded.size(); j++) {
-                if(textures_loaded[j].path == str)
-                {
+                if(textures_loaded[j].path == str) {
                     textures.push_back(textures_loaded[j]);
-                    skip = true; // A texture with the same filepath has already been loaded, continue to next one. (optimization)
+                    skip = true;
                     break;
                 }
             }
-            if(!skip) {   // If texture hasn't been loaded already, load it
+            if(!skip) {
                 Texture texture;
                 texture.id = TextureFromFile(str.C_Str(), this->directory);
                 texture.type = typeName;
                 texture.path = str;
                 textures.push_back(texture);
-                this->textures_loaded.push_back(texture);  // Store it as texture loaded for entire model, to ensure we won't unnecesery load duplicate textures.
+                this->textures_loaded.push_back(texture);
             }
         }
         return textures;
     }
 };
-
-double mod(double x, double y) {
-    int sign = 1;
-    if (x<0) {
-        x = -x;
-        sign = -1;
-    }
-    return  sign * (int( x / y ) % 2 ? y - (x - y * int( x / y )) : (x - y * int( x / y ))) ;
-}
 
 const uint GRASS_INSTANCES = 30000; // Количество травинок
 const uint ROSES_INSTANCES = 50;    // Количество роз
@@ -445,6 +366,15 @@ void DrawSkyBox() {
     glBindVertexArray(0);                                                        CHECK_GL_ERRORS
     glUseProgram(0);                                                             CHECK_GL_ERRORS
     glDepthMask(GL_TRUE);
+}
+
+double mod(double x, double y) {
+    int sign = 1;
+    if (x<0) {
+        x = -x;
+        sign = -1;
+    }
+    return  sign * (int( x / y ) % 2 ? y - (x - y * int( x / y )) : (x - y * int( x / y ))) ;
 }
 
 void UpdateGrassRandomTilting() {
